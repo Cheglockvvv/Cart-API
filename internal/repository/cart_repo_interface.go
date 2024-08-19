ff1353bf560686f5347fb4b9a8bb183cc0857aa3
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-19 14:35:37 +0300

    repo interface declaration

--- internal/repository/cart_repo_interface.go
+++ internal/repository/cart_repo_interface.go
@@ -1,5 +1,14 @@
 package repository
 
+import (
+	"Cart-API/internal/models"
+	"github.com/jmoiron/sqlx"
+)
+
 type CartRepoInterface interface {
-	init()
+	Init(dataSourceName string) (*sqlx.DB, error)
+	CreateCart() (string, error)
+	GetCartByID(cartID string) (*models.Cart, error)
+	AddItemToCart(cartID string, item *models.CartItem) (string, error)
+	RemoveItemFromCart(cartID, itemID string) error
 }
