439e719195867b5dba0ea914f76ba443c571359b
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-20 18:15:08 +0300

    routes

--- cmd/main.go
+++ cmd/main.go
@@ -10,18 +10,23 @@ import (
 
 func main() {
 	cartRepository := repository.PostgresCart{}
-	err := cartRepository.Init("fasd")
+	err := cartRepository.Init(
+		"postgres://postgres:418032@localhost:5432/postgres?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	cartService := service.NewCart(&cartRepository)
-
 	cartHandler := handler.NewHandler(cartService)
 
 	mux := http.NewServeMux()
-	mux.Handle("POST /cart/{id}", cartHandler)
-	mux.HandleFunc("lksgjaslg", cartHandler.CreateCart)
+	mux.HandleFunc("POST /cart", cartHandler.CreateCart)
+	mux.HandleFunc("POST /cart/{id}/items", cartHandler.AddItemToCart)
+	mux.HandleFunc("DELETE /cart/{cartID}/items/{itemID}", cartHandler.RemoveItemFromCart)
+	mux.HandleFunc("GET /cart/{id}", cartHandler.GetCartByID)
 
-	http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
