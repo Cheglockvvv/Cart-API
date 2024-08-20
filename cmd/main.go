b9509ae5cffe286c23c835cba3d0ce9f79910930
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-20 14:51:08 +0300

    router init

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "Cart-API/internal/repository"
+import (
+	"Cart-API/internal/handler"
+	"Cart-API/internal/repository"
+	"Cart-API/internal/service"
+	"log"
+	"net/http"
+)
 
 func main() {
-	repository.PostgresCart{}
+	cartRepository := repository.PostgresCart{}
+	err := cartRepository.Init("fasd")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cartService := service.NewService(&cartRepository)
+
+	cartHandler := handler.NewHandler(cartService)
+
+	mux := http.NewServeMux()
+	mux.Handle("/cart", cartHandler)
+
+	http.ListenAndServe(":8080", mux)
 }
