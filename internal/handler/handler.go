55957f35f286d3768a78aca41a93f4a44764712e
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-20 13:44:40 +0300

    service interface

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,14 @@
 package handler
+
+import "Cart-API/internal/models"
+
+type CartService interface {
+	CreateCart() (string, error)
+	GetCartByID(cartID string) (*models.Cart, error)
+	AddItemToCart(cartID, name string, quantity int) error
+	RemoveItemFromCart(cartID, itemID string) error
+}
+
+type Cart struct {
+	cartService CartService
+}
