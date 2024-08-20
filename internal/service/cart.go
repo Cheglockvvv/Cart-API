401dece8b9be230c3bc1c08d0b6467b710d2d45a
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-20 13:38:51 +0300

    service implementation

---
+++ internal/service/cart.go
@@ -0,0 +1,48 @@
+package service
+
+import "Cart-API/internal/models"
+
+type CartRepository interface {
+	Init(dataSourceName string) error
+	CreateCart() (string, error)
+	GetCartByID(id string) (*models.Cart, error)
+	AddItemToCart(cartID, name string, quantity int) (string, error)
+	RemoveItemFromCart(cartID, itemID string) error
+}
+
+type Cart struct {
+	cartRepository CartRepository
+}
+
+func (service *Cart) NewCart() (string, error) {
+	id, err := service.cartRepository.CreateCart()
+	if err != nil {
+		return "", err
+	}
+	return id, nil
+}
+
+func (service *Cart) GetCartByID(id string) (*models.Cart, error) {
+	cart, err := service.cartRepository.GetCartByID(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cart, nil
+}
+
+func (service *Cart) AddItemToCart(cartID, name string, quantity int) (string, error) {
+	itemID, err := service.cartRepository.AddItemToCart(cartID, name, quantity)
+
+	if err != nil {
+		return "", err
+	}
+
+	return itemID, nil
+}
+
+func (service *Cart) RemoveItemFromCart(cartID, itemID string) error {
+	err := service.cartRepository.RemoveItemFromCart(cartID, itemID)
+	return err
+}
