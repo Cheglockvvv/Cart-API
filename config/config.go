bb3470a17d7f6c058c982e84327535e28fed1727
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-21 17:27:17 +0300

    add config struct

--- config/config.go
+++ config/config.go
@@ -1,1 +1,57 @@
 package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type APIConfig struct {
+	Port string
+}
+
+type DBConfig struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	DBName   string
+	SSLMode  string
+}
+
+type Cart struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+func LoadEnv() (*Cart, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("godotenv.Load")
+	}
+
+	requiredEnvs := []string{
+		"DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "DB_PORT", "API_PORT", "DB_SSLMODE"}
+
+	for _, env := range requiredEnvs {
+		if os.Getenv(env) == "" {
+			return nil, fmt.Errorf("missing environment variable %s", env)
+		}
+	}
+
+	config := Cart{
+		API: APIConfig{
+			Port: os.Getenv("API_PORT"),
+		},
+		DB: DBConfig{
+			Host:     os.Getenv("DB_HOST"),
+			User:     os.Getenv("DB_USER"),
+			Password: os.Getenv("DB_PASSWORD"),
+			DBName:   os.Getenv("DB_NAME"),
+			Port:     os.Getenv("DB_PORT"),
+			SSLMode:  os.Getenv("DB_SSLMODE"),
+		},
+	}
+
+	return &config, nil
+}
