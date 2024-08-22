7bc617584043a570391ccbfea19d57f81d40d109
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-22 17:25:49 +0300

    add migrations to main

---
+++ app/cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"Cart-API/app/config"
+	"Cart-API/app/internal/db/migrations"
+	"Cart-API/app/internal/handler"
+	"Cart-API/app/internal/repository"
+	"Cart-API/app/internal/service"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	cfg, err := config.LoadEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const DSN = "postgres://%s:%s@%s:%s/%s?sslmode=%s"
+	filledDsn := fmt.Sprintf(DSN, cfg.DB.User, cfg.DB.Password, cfg.DB.Host, cfg.DB.Port,
+		cfg.DB.DBName, cfg.DB.SSLMode)
+
+	cartRepository := repository.PostgresCart{}
+	err = cartRepository.Init(filledDsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = migrations.Up(cartRepository.DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cartService := service.NewCart(&cartRepository)
+	cartHandler := handler.NewHandler(cartService)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /cart", cartHandler.CreateCart)
+	mux.HandleFunc("POST /cart/{id}/items", cartHandler.AddItemToCart)
+	mux.HandleFunc("DELETE /cart/{cartID}/items/{itemID}", cartHandler.RemoveItemFromCart)
+	mux.HandleFunc("GET /cart/{id}", cartHandler.GetCartByID)
+
+	err = http.ListenAndServe(fmt.Sprintf(":%s", cfg.API.Port), mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
