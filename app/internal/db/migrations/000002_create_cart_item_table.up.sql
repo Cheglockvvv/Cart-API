4e5f759615fe96dc0a79467f3add94a2cda67c4a
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-22 17:26:28 +0300

    migrations

---
+++ app/internal/db/migrations/000002_create_cart_item_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS cart_item (
+    id BIGSERIAL PRIMARY KEY,
+    cart_id BIGINT NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    quantity BIGINT NOT NULL,
+    FOREIGN KEY (id) REFERENCES cart (id)
+);
