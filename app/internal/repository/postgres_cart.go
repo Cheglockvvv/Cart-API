9d6f66f7b5214a62ec7987b1d0b733058099dc20
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-23 13:39:40 +0300

    getItem method and cartIsAvailable

--- app/internal/repository/postgres_cart.go
+++ app/internal/repository/postgres_cart.go
@@ -1,7 +1,7 @@
 package repository
 
 import (
-	models2 "Cart-API/app/internal/models"
+	models "Cart-API/app/internal/models"
 	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
@@ -40,24 +40,27 @@ func (r *PostgresCart) CreateCart() (string, error) {
 	return id, nil
 }
 
-func (r *PostgresCart) GetCartByID(id string) (models2.Cart, error) {
+func (r *PostgresCart) GetCartByID(id string) (models.Cart, error) {
+	ok, err := r.cartIsAvailable(id)
+	if err != nil {
+		return models.Cart{}, fmt.Errorf("r.cartIsAvailable: %w", err)
+	}
+
+	if !ok {
+		return models.Cart{}, fmt.Errorf("cart not found")
+	}
 	const query = `SELECT ci.id, ci.cart_id, ci.name, ci.quantity 
 								FROM cart_item ci 
 								WHERE ci.cart_id = $1`
 
 	rows, err := r.DB.Queryx(query, id)
 	if err != nil {
-		return models2.Cart{}, fmt.Errorf("r.DB.Queryx: %w", err)
+		return models.Cart{}, fmt.Errorf("r.DB.Queryx: %w", err)
 	}
 	defer rows.Close()
 
-	if !rows.Next() {
-		return models2.Cart{}, nil
-	}
+	cart := models.Cart{ID: id}
 
-	cart := models2.Cart{ID: id}
-
-	//TODO: change struct
 	items := make([]cartItem, 0)
 
 	for rows.Next() {
@@ -65,12 +68,12 @@ func (r *PostgresCart) GetCartByID(id string) (models2.Cart, error) {
 		err = rows.StructScan(&row)
 
 		if err != nil {
-			return models2.Cart{}, fmt.Errorf("rows.StructScan: %w", err)
+			return models.Cart{}, fmt.Errorf("rows.StructScan: %w", err)
 		}
 		items = append(items, row)
 	}
 
-	convertedItems := make([]models2.CartItem, len(items))
+	convertedItems := make([]models.CartItem, len(items))
 	for i := range items {
 		convertedItems[i] = modelConvert(items[i])
 	}
@@ -79,56 +82,55 @@ func (r *PostgresCart) GetCartByID(id string) (models2.Cart, error) {
 	return cart, nil
 }
 
-func (r *PostgresCart) AddItemToCart(cartID, name string, quantity int) (string, error) {
-	const check = `SELECT id FROM cart WHERE id = $1`
-	result, err := r.DB.Exec(check, cartID)
+func (r *PostgresCart) AddItemToCart(cartID, name string, quantity int) (models.CartItem, error) {
+
+	ok, err := r.cartIsAvailable(cartID)
 	if err != nil {
-		return "", fmt.Errorf("r.DB.Exec: %w", err)
-	}
-	count, err := result.RowsAffected()
-	if err != nil {
-		return "", fmt.Errorf("result.RowsAffected: %w", err)
+		return models.CartItem{}, fmt.Errorf("r.cartIsAvailable: %w", err)
 	}
 
-	if count != 1 {
-		return "", fmt.Errorf("cart not found")
+	if !ok {
+		return models.CartItem{}, fmt.Errorf("cart not found")
 	}
 
 	const query = `INSERT INTO cart_item (cart_id, name, quantity)
 								VALUES ($1, $2, $3)
+								ON CONFLICT (cart_id, name)
+								DO UPDATE SET quantity = cart_item.quantity +
+								    EXCLUDED.quantity
 								RETURNING id`
 	var itemID string
 	err = r.DB.QueryRowx(query, cartID, name, quantity).Scan(&itemID)
 
 	if err != nil {
-		return "", fmt.Errorf("r.DB.QueryRowx: %w", err)
+		return models.CartItem{}, fmt.Errorf("r.DB.QueryRowx: %w", err)
 	}
 
-	return itemID, nil
+	item, err := r.GetItem(itemID)
+	if err != nil {
+		return models.CartItem{}, fmt.Errorf("r.GetItem: %w", err)
+	}
+
+	return item, nil
 }
 
 func (r *PostgresCart) RemoveItemFromCart(cartID, itemID string) error {
 
-	const checkCart = `SELECT id FROM cart WHERE id = $1`
-	result, err := r.DB.Exec(checkCart, cartID)
+	ok, err := r.cartIsAvailable(cartID)
 	if err != nil {
-		return fmt.Errorf("r.DB.Exec: %w", err)
-	}
-	count, err := result.RowsAffected()
-	if err != nil {
-		return fmt.Errorf("result.RowsAffected: %w", err)
+		return fmt.Errorf("r.cartIsAvailable: %w", err)
 	}
 
-	if count != 1 {
+	if !ok {
 		return fmt.Errorf("cart not found")
 	}
 
 	const checkItem = `SELECT id FROM cart_item WHERE id = $1`
-	result, err = r.DB.Exec(checkItem, itemID)
+	result, err := r.DB.Exec(checkItem, itemID)
 	if err != nil {
 		return fmt.Errorf("r.DB.Exec: %w", err)
 	}
-	count, err = result.RowsAffected()
+	count, err := result.RowsAffected()
 	if err != nil {
 		return fmt.Errorf("result.RowsAffected: %w", err)
 	}
@@ -144,8 +146,43 @@ func (r *PostgresCart) RemoveItemFromCart(cartID, itemID string) error {
 	return nil
 }
 
-func modelConvert(item cartItem) models2.CartItem {
-	modelItem := models2.CartItem{
+func (r *PostgresCart) cartIsAvailable(id string) (bool, error) {
+
+	const checkCart = `SELECT id FROM cart WHERE id = $1`
+	result, err := r.DB.Exec(checkCart, id)
+	if err != nil {
+		return false, fmt.Errorf("r.DB.Exec: %w", err)
+	}
+	count, err := result.RowsAffected()
+	if err != nil {
+		return false, fmt.Errorf("result.RowsAffected: %w", err)
+	}
+
+	if count != 1 {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (r *PostgresCart) GetItem(id string) (models.CartItem, error) {
+	const query = `SELECT ci.id, ci.cart_id, ci.name, ci.quantity 
+								FROM cart_item ci 
+								WHERE ci.id = $1`
+	result := r.DB.QueryRowx(query, id)
+	item := cartItem{}
+	err := result.StructScan(&item)
+	if err != nil {
+		return models.CartItem{}, fmt.Errorf("result.StructScan: %w", err)
+	}
+
+	convertedItem := modelConvert(item)
+
+	return convertedItem, nil
+}
+
+func modelConvert(item cartItem) models.CartItem {
+	modelItem := models.CartItem{
 		ID:       item.ID,
 		CartID:   item.CartID,
 		Name:     item.Name,
