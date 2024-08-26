7c5f400c64a47ae008ee8da0aef9f518d98f24cf
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-26 18:47:15 +0300

    CartItemRepository interface declaration

--- app/internal/service/cart_item.go
+++ app/internal/service/cart_item.go
@@ -1,1 +1,10 @@
 package service
+
+import "Cart-API/app/internal/models"
+
+type CartItemRepository interface {
+	AddItemToCart(cartID, name string, quantity int) (string, error)
+	GetItemByID(id string) (models.CartItem, error)
+	RemoveItemFromCart(cartID, itemID string) error
+	ItemIsAvailable(id string) (bool, error)
+}
