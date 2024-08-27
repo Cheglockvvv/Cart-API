3c0f8bb658d1fd28143a2805056ba85a6a543edf
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-27 15:20:19 +0300

    first test not ready

--- app/internal/service/cart_test.go
+++ app/internal/service/cart_test.go
@@ -1,1 +1,32 @@
 package service
+
+import (
+	"Cart-API/app/internal/models"
+	_ "github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestCreateCart(t *testing.T) {
+	tests := []struct {
+		name          string
+		mockCart      *models.Cart
+		mockError     error
+		expectedCart  *models.Cart
+		expectedError error
+	}{
+		{
+			name:          "Success Test",
+			mockCart:      &models.Cart{ID: "1", Items: []models.CartItem{}},
+			mockError:     nil,
+			expectedCart:  &models.Cart{ID: "1", Items: []models.CartItem{}},
+			expectedError: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockRepo := new(mocks.Repository)
+		})
+	}
+}
