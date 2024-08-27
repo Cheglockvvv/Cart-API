030f6675a7ecd49e230d9687d0af3846d60c9715
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-27 14:25:40 +0300

    remove CartItem functions from Cart repository

--- app/internal/repository/cart.go
+++ app/internal/repository/cart.go
@@ -68,35 +68,6 @@ func (c *Cart) GetCartByID(id string) (models.Cart, error) {
 	return cart, nil
 }
 
-func (c *Cart) AddItemToCart(cartID, name string, quantity int) (string, error) {
-
-	const query = `INSERT INTO cart_item (cart_id, product, quantity)
-								VALUES ($1, $2, $3)
-								ON CONFLICT (cart_id, product)
-								DO UPDATE SET quantity = cart_item.quantity +
-								    EXCLUDED.quantity
-								RETURNING id`
-	var itemID string
-	err := c.DB.QueryRowx(query, cartID, name, quantity).Scan(&itemID)
-
-	if err != nil {
-		return "", fmt.Errorf("c.DB.QueryRowx: %w", err)
-	}
-
-	return itemID, nil
-}
-
-func (c *Cart) RemoveItemFromCart(cartID, itemID string) error {
-	const query = `DELETE FROM cart_item WHERE cart_id = $1 AND id = $2`
-
-	_, err := c.DB.Exec(query, cartID, itemID)
-	if err != nil {
-		return fmt.Errorf("c.DB.Exec: %w", err)
-	}
-
-	return nil
-}
-
 func (c *Cart) CartIsAvailable(id string) (bool, error) {
 
 	const checkCart = `SELECT id FROM cart WHERE id = $1`
@@ -114,38 +85,4 @@ func (c *Cart) CartIsAvailable(id string) (bool, error) {
 	}
 
 	return true, nil
-}
-
-func (c *Cart) ItemIsAvailable(id string) (bool, error) {
-	const checkItem = `SELECT id FROM cart_item WHERE id = $1`
-	result, err := c.DB.Exec(checkItem, id)
-	if err != nil {
-		return false, fmt.Errorf("c.DB.Exec: %w", err)
-	}
-	count, err := result.RowsAffected()
-	if err != nil {
-		return false, fmt.Errorf("result.RowsAffected: %w", err)
-	}
-
-	if count != 1 {
-		return false, nil
-	}
-
-	return true, nil
-}
-
-func (c *Cart) GetItemByID(id string) (models.CartItem, error) {
-	const query = `SELECT ci.id, ci.cart_id, ci.product, ci.quantity 
-								FROM cart_item ci 
-								WHERE ci.id = $1`
-	result := c.DB.QueryRowx(query, id)
-	item := cartItemEntity{}
-	err := result.StructScan(&item)
-	if err != nil {
-		return models.CartItem{}, fmt.Errorf("result.StructScan: %w", err)
-	}
-
-	convertedItem := cartItemConvert(item)
-
-	return convertedItem, nil
 }
