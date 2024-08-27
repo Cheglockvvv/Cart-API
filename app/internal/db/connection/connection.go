81993dda9c14dd544d0bf39331769f544b66bf3b
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-27 14:10:44 +0300

    add getConnection function to get DB and then put it into repositories

---
+++ app/internal/db/connection/connection.go
@@ -0,0 +1,15 @@
+package connection
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+func getConnection(connectionString string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("pgx", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("sqlx.Connect: %w", err)
+	}
+
+	return db, nil
+}
