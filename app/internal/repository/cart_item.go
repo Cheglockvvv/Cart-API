24ce190c76e5e86c549cd999ee4aa6f8f975a51b
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-27 14:22:13 +0300

    add AddItemToCart function to CartItem repository

--- app/internal/repository/cart_item.go
+++ app/internal/repository/cart_item.go
@@ -1,6 +1,9 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
 
 type CartItem struct {
 	DB *sqlx.DB
@@ -10,4 +13,22 @@ func InitCartItem(db *sqlx.DB) *CartItem {
 	cartItem := &CartItem{DB: db}
 
 	return cartItem
+}
+
+func (c *CartItem) AddItemToCart(cartID, name string, quantity int) (string, error) {
+
+	const query = `INSERT INTO cart_item (cart_id, product, quantity)
+								VALUES ($1, $2, $3)
+								ON CONFLICT (cart_id, product)
+								DO UPDATE SET quantity = cart_item.quantity +
+								    EXCLUDED.quantity
+								RETURNING id`
+	var itemID string
+	err := c.DB.QueryRowx(query, cartID, name, quantity).Scan(&itemID)
+
+	if err != nil {
+		return "", fmt.Errorf("c.DB.QueryRowx: %w", err)
+	}
+
+	return itemID, nil
 }
