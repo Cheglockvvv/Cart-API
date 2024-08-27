b6440d97cabd43a796ca0f0fa0a4ed26114e28fb
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-27 14:20:35 +0300

    create InitCartItem function

--- app/internal/repository/cart_item.go
+++ app/internal/repository/cart_item.go
@@ -1,4 +1,13 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type CartItem struct {
+	DB *sqlx.DB
+}
+
+func InitCartItem(db *sqlx.DB) *CartItem {
+	cartItem := &CartItem{DB: db}
+
+	return cartItem
 }
