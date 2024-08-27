cabba5124a0d02c2ead689303dd94efebc144ecb
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-27 14:23:22 +0300

    add ItemIsAvailable and GetItemByID function to repository

--- app/internal/repository/cart_item.go
+++ app/internal/repository/cart_item.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"Cart-API/app/internal/models"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 )
@@ -42,4 +43,38 @@ func (c *CartItem) RemoveItemFromCart(cartID, itemID string) error {
 	}
 
 	return nil
+}
+
+func (c *CartItem) ItemIsAvailable(id string) (bool, error) {
+	const checkItem = `SELECT id FROM cart_item WHERE id = $1`
+	result, err := c.DB.Exec(checkItem, id)
+	if err != nil {
+		return false, fmt.Errorf("c.DB.Exec: %w", err)
+	}
+	count, err := result.RowsAffected()
+	if err != nil {
+		return false, fmt.Errorf("result.RowsAffected: %w", err)
+	}
+
+	if count != 1 {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (c *CartItem) GetItemByID(id string) (models.CartItem, error) {
+	const query = `SELECT ci.id, ci.cart_id, ci.product, ci.quantity 
+								FROM cart_item ci 
+								WHERE ci.id = $1`
+	result := c.DB.QueryRowx(query, id)
+	item := cartItemEntity{}
+	err := result.StructScan(&item)
+	if err != nil {
+		return models.CartItem{}, fmt.Errorf("result.StructScan: %w", err)
+	}
+
+	convertedItem := cartItemConvert(item)
+
+	return convertedItem, nil
 }
