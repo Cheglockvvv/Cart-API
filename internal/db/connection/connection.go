8d9bf790242b55008b355790ea76045c927b63cd
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-29 17:08:27 +0300

    switch pgx to postgres (works)

--- internal/db/connection/connection.go
+++ internal/db/connection/connection.go
@@ -11,7 +11,7 @@ func GetConnection(config config.DBConfig) (*sqlx.DB, error) {
 	postgresConnectionString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
 		config.User, config.Password, config.Host, config.Port, config.DBName, config.SSLMode)
 
-	db, err := sqlx.Connect("pgx", postgresConnectionString)
+	db, err := sqlx.Connect("postgres", postgresConnectionString)
 	if err != nil {
 		return nil, fmt.Errorf("sqlx.Connect: %w", err)
 	}
