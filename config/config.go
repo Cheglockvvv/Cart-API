4d3c18d2e379f14edb3bf037bec73a2382bbbe9d
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-08-29 12:23:51 +0300

    switch to Carlos env parser

---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"github.com/caarlos0/env"
+)
+
+type APIConfig struct {
+	Port string `env:"API_PORT"`
+}
+
+type DBConfig struct {
+	User     string `env:"DB_USER"`
+	Password string `env:"DB_PASSWORD"`
+	Host     string `env:"DB_HOST"`
+	Port     string `env:"DB_PORT"`
+	DBName   string `env:"DB_NAME"`
+	SSLMode  string `env:"SSL_MODE"`
+}
+
+type Cart struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+func LoadEnv() (*Cart, error) {
+	var dbConfig DBConfig
+	err := env.Parse(&dbConfig)
+	if err != nil {
+		return nil, fmt.Errorf("env.Parse: %w", err)
+	}
+
+	var apiConfig APIConfig
+	err = env.Parse(&apiConfig)
+	if err != nil {
+		return nil, fmt.Errorf("env.Parse: %w", err)
+	}
+
+	config := Cart{
+		API: apiConfig,
+		DB:  dbConfig,
+	}
+
+	return &config, nil
+}
